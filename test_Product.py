613833ee28d05e88965b0a411c15c3e94bca9914
Swapna-Karlawad <swapnakarlawad@gmail.com>
2025-12-11 15:51:17 +0530

    Update test_Product.py

--- test_Product.py
+++ test_Product.py
@@ -4,7 +4,7 @@ def test_product_details():
     expected_output = (
         "Product Name: Phone\n"
         "Product ID: 1001\n"
-        "Quantity:1\n"
+        "Quantity: 1\n"
         "Price: 10000\n"
     )
     assert product_details("Phone", "1001", "1", 10000) == expected_output
